3055dd8ea5d4f78578aa15608f542388c9bb1457
Rachelkakoderka <rachelkakoderka@gmail.com>
2023-03-23 14:11:42 +0100

    first grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+
+
 
 function App() {
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h2>ClassicalArt Memory Game</h2>
       </header>
+      <div className='main'>
+        <div className="level_container">
+
+        </div>
+        <div className="stats_container">
+
+        </div>
+        <div className="board_container">
+          <Card  />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          
+        </div>
+      </div>
+      <footer>
+        Created by <a href='www.aleksandragalach.com'> Aleksandra Gałach </a> 2023
+      </footer>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import four from "../assets/four.jpg" 
+
+export default function Card() {
+  return (
+    <>
+        <div className='card'>
+            <img className='card_img' src={four} />
+        </div>  
+    </>
+    )
+}
